3225ca787a2b7597858f4e08eb85c126d3a8353d
Tosin Akinlua <akinluarotimi@gmail.com>
2021-11-04 02:28:33 +0100

    Set up basic functions for operations

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function displayNumber(number) {
         }
     });
 }
+
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n-1);
+}
+
+const sum = (a, b) => (a * 10 + b * 10) / 10;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const power = (a, b) => Math.pow(a, b);
+const squareRoot = n => Math.sqrt(n);
